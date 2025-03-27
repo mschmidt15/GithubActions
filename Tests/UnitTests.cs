48ddcaea5b0fac974c960a3bc7fd17be9bf5d62b
mschmidt15 <mschmidt52@unl.edu>
2025-03-27 14:59:33 -0500

    Added tests for all functions, purposeful subtraction error

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,113 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+[TestClass]
+public class Subtraction
+{
+    [TestMethod]
+    public void Subtract_Valid_Patino()
+    {
+        Assert.AreEqual(7, Program.Subtract("8", "1"));
+        Assert.AreEqual(2, Program.Subtract("3", "2"));
+        Assert.AreEqual(13, Program.Subtract("20", "7"));
+    }
+
+    [TestMethod]
+    public void Subtract_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+    }
+
+    [TestMethod]
+    public void Subtract_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+    }
+}
+
+[TestClass]
+public class Multiplication
+{
+    [TestMethod]
+    public void Multiply_Valid_Patino()
+    {
+        Assert.AreEqual(8, Program.Multiply("4", "2"));
+        Assert.AreEqual(0, Program.Multiply("3", "0"));
+        Assert.AreEqual(36, Program.Multiply("12", "3"));
+    }
+
+    [TestMethod]
+    public void Multiply_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+    }
+
+    [TestMethod]
+    public void Multiply_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+    }
+}
+
+[TestClass]
+public class Division
+{
+    [TestMethod]
+    public void Divide_Valid_Patino()
+    {
+        Assert.AreEqual(8, Program.Divide("8", "1"));
+        Assert.AreEqual(0, Program.Divide("0", "2"));
+        Assert.AreEqual(10, Program.Divide("20", "2"));
+    }
+
+    [TestMethod]
+    public void Dividet_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+    }
+
+    [TestMethod]
+    public void Divide_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+    }
+}
+
+[TestClass]
+public class Exponential
+{
+    [TestMethod]
+    public void Power_Valid_Patino()
+    {
+        Assert.AreEqual(8, Program.Power("2", "3"));
+        Assert.AreEqual(1, Program.Power("3", "0"));
+        Assert.AreEqual(64, Program.Power("4", "3"));
+    }
+    [TestMethod]
+    public void Power_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+    }
+    [TestMethod]
+    public void Power_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+    }
+}
